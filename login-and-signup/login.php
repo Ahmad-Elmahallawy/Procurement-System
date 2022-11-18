306e76f8a72fc5e0ab8f388e56e11153979c14e8
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2022-11-18 18:35:26 -0500

    half done with user registration

--- login-and-signup/login.php
+++ login-and-signup/login.php
@@ -1,6 +1,41 @@
+<?php
+@include 'config.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = md5($_POST['password']);
+
+
+    $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass'  ";
+
+    $result = mysqli_query($conn, $select);
+    if(mysqli_num_rows($result) > 0)
+    {
+        $row = mysqli_fetch_array($result);
+
+        if($row['user_type'] == 'admin'){
+          $_SESSION['admin_name']  = $row['name'];
+          header('location:./users/userlist.php');
+        }
+        elseif($row['user_type'] == 'customer'){
+          $_SESSION['user_name'] = $row['name'];
+          header('location:../index.php');
+    }
+        else{
+          $error[] = "incorrect email or password!";
+        }
+    
+    
+}
+};
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,23 +44,23 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <style>
-
-      body{
+    body {
         margin: 0 0;
-      }
-      .form-container{
-        height: 350px;
-      }
+    }
 
-      .wrapper{
-        display:flex;
+    .form-container {
+        height: 350px;
+    }
+
+    .wrapper {
+        display: flex;
         align-items: center;
-      }
-      
+    }
     </style>
-  </head>
-  <body>
-  <?php
+</head>
+
+<body>
+    <?php
     require_once ('../header/header.php');
     ?>
     <div class="wrapper">
@@ -33,23 +68,26 @@
             <div class="form-container">
                 <div class="title">
                     <h3>Login</h3>
+                    <?php
+                    if(isset($error)){
+                        foreach($error as $error){
+                            echo '<span class="error-msg">' .$error. '</span>';
+                    };
+                };
+                ?>
                 </div>
                 <div class="inputs">
-                    <input type="text" required id="contact-email" onkeydown="validateEmail()">
+                    <input type="text" name="email" required id="contact-email" onkeydown="validateEmail()">
                     <label>Email</label>
                     <span id="email-error"></span>
                 </div>
                 <div class="inputs">
-                    <input type="password" required id="pass1" onkeydown="validatePassword()">
+                    <input type="password" name="password" required id="pass1" onkeydown="validatePassword()">
                     <label>Password</label>
                     <span id="password-error"></span>
                 </div>
                 <div class="btn">
-                    <input
-                    type="submit"
-                    name="submit"
-                    value="Login"
-                    class="form-btn">
+                    <input type="submit" name="submit" value="Login" class="form-btn">
                     <p>Don't have an account? <a href="signup.php">Signup</a></p>
                     <span id="submit-error"></span>
                 </div>
@@ -57,5 +95,6 @@
         </form>
     </div>
     <script src="registration_validation.js"></script>
-  </body>
+</body>
+
 </html>
--- login-and-signup/signup.php
+++ login-and-signup/signup.php
@@ -1,61 +1,106 @@
+<?php
+@include 'config.php';
+if(isset($_POST['submit'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = md5($_POST['password']);
+    $cpass = md5($_POST['cpassword']);
+    $user_type = $_POST['user_type'];
+
+    $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass'  ";
+
+    $result = mysqli_query($conn, $select);
+    if(mysqli_num_rows($result) > 0)
+    {
+        $error[] = 'user already exists';
+    }
+    else{
+        if($pass != $cpass)
+        {
+            $error[] = 'password not matched!';
+        }
+        else{
+            $insert = "INSERT INTO user_form(name,email,password,user_type) VALUES('$name','$email','$pass','$user_type')";
+            mysqli_query($conn,$insert);
+            header('location:login.php');
+        }
+    }
+};
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Signup form</title>
     <link rel="stylesheet" href="registration_style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
-  <body>
-  <?php
+</head>
+
+<body>
+    <?php
     require_once ('../header/header.php');
 ?>
-        <div class="wrapper">
-            <div class="form-container">
+    <div class="wrapper">
+        <div class="form-container">
+            <form action="" method="post">
+
                 <div class="title">
                     <h3>Signup</h3>
+                    <?php
+                    if(isset($error)){
+                        foreach($error as $error){
+                            echo '<span class="error-msg">' .$error. '</span>';
+                    };
+                };
+                ?>
+
                 </div>
+
                 <div class="inputs">
-                    <input type="text" required id="contact-name" onkeyup="validateName()">
+                    <input type="text" name="name" required id="contact-name" onkeyup="validateName()">
                     <label>Name</label>
                     <span id="name-error"></span>
                 </div>
                 <div class="inputs">
-                    <input type="email" required id="contact-email" onkeyup="validateEmail()"">
-                    <label id="email-label">Email</label>
+                    <input type="email" name="email" required id="contact-email" onkeyup="validateEmail()"">
+                    <label id=" email-label">Email</label>
                     <span id="email-error"></span>
                 </div>
                 <div class="inputs">
-                    <input type="password" required id="pass1" onkeydown="validatePassword()">
+                    <input type="password" name="password" required id="pass1" onkeydown="validatePassword()">
                     <label>Password</label>
                     <span id="password-error"></span>
                 </div>
                 <div class="inputs">
-                    <input type="password" required id="pass2" onkeydown="validatePassword2()">
+                    <input type="password" name="cpassword" required id="pass2" onkeydown="validatePassword2()">
                     <label>Confirm Passowrd</label>
                     <span id="cpassword-error"></span>
                 </div>
                 <div class="inputs ">
                     <label class="choice">You are a:</label>
-                    <select name="user" id="user">
+                    <select name="user_type" id="user">
                         <option value="customer">Customer</option>
                         <option value="suppler">Supplier</option>
                         <option value="customer">Supervisor</option>
                     </select>
                 </div>
                 <div class="btn">
-                    <input
-                    type="submit"
-                    name="submit"
-                    value="Signup"
-                    class="form-btn">
+                    <input type="submit" name="submit" value="Signup" class="form-btn">
                     <p>Already have an account? <a href="login.php">Login</a></p>
                 </div>
-            </div>
+            </form>
         </div>
-   
+    </div>
+
     <script src="registration_validation.js"></script>
-  </body>
+</body>
+
 </html>
