ac2e185b3329b7eb02a00a072d42fcbb35972352
Karinsarkis98 <91097032+Karinsarkis98@users.noreply.github.com>
2022-12-01 17:10:07 -0500

    fixing cart and status

--- cart/cart.php
+++ cart/cart.php
@@ -1,42 +1,7 @@
 <?php
+//session_start();
 
-include '../backstore/connection2.php';
-session_start();
-// $user_id = $_SESSION['user_id'];
-
-// if(!isset($user_id)){
-//    header('location:../login.php');
-// };
-
-// if(isset($_GET['logout'])){
-//    unset($user_id);
-//    session_destroy();
-//    header('location:./login.php');
-// };
-
-$user_name = $_SESSION['user_name'];
-
-// Clear the cart after it is rejected
-if(isset($_GET['rejected'])){
-  mysqli_query($conn, "DELETE FROM `cart` WHERE user_name = '$user_name'") or die('query failed');
-
-  //send message to the user I was thinking maybe we make another field ??
-  mysqli_query($conn, "UPDATE `user_form` SET quote = 'rejected'") or die('query failed');
-
-  // go to the rfq pages to approve/reject your next quotation
-  header('location:../index.php');
-}
-
-// Clear the cart after it is approved
-if(isset($_GET['rejected'])){
-  mysqli_query($conn, "DELETE FROM `cart` WHERE user_name = '$user_name'") or die('query failed');
-
-  //send message to the user I was thinking maybe we make another field ??
-  mysqli_query($conn, "UPDATE `user_form` SET quote = 'approved'") or die('query failed');
-
-  // go to the rfq pages to approve/reject your next quotation
-  header('location:../index.php');
-}
+$user_name = "karin";
 
 ?>
 
@@ -53,7 +18,7 @@ if(isset($_GET['rejected'])){
 
 </head>
 <body>
-   
+
 <?php
 require_once ('../header/header.php');
 ?>
@@ -66,23 +31,36 @@ require_once ('../header/header.php');
 
    <table>
       <thead>
+
          <th>Image</th>
          <th>Product</th>
          <th>Quantity</th>
          <th>Price</th>
+         <th>Status</th>
       </thead>
       <tbody>
       <?php
-         $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_name = 'karin'") or die('query failed');
+         $hostName = 'localhost';
+         $dbUsername = 'root';
+         $dbPassword = '';
+         $dbName = "soen341_db";
+         
+         $conn = mysqli_connect($hostName,$dbUsername,$dbPassword,$dbName);
+
+         $cart_query = mysqli_query($conn, "SELECT * FROM cart WHERE user_name = 'karin'") or die('query failed');
          $grand_total = 0;
          if(mysqli_num_rows($cart_query) > 0){
             while($fetch_cart = mysqli_fetch_assoc($cart_query)){
       ?>
+
+      
          <tr>
-            <td><img src="../images/<?php echo $fetch_cart['image']; ?>" height="100" alt=""></td>
-            <td><?php echo $fetch_cart['product_name']; ?></td>
-            <td><?php echo $fetch_cart['quantity']; ?></td>
-            <td>$<?php echo $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?></td>
+              
+            <td><img src="images/<?php echo $fetch_cart['image']; ?>" height="100" alt=""><?php echo ($fetch_cart['image'] == NULL)?'Unavailable':''?></td>
+            <td><?php echo $fetch_cart['product_name'];?></td>
+            <td><?php echo $fetch_cart['quantity'];?></td>
+            <td>$<?php echo $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']);?></td>
+            <td><?php echo $fetch_cart['status'];?></td>
          </tr>
       <?php
          $grand_total += $sub_total;
@@ -91,33 +69,13 @@ require_once ('../header/header.php');
             echo '<tr><td style="padding:20px; text-transform:capitalize;" colspan="6">No Items</td></tr>';
          }
       ?>
-      <tr class="table-bottom">
-         <td colspan="3" style="text-align: right; padding-right: 5em">Final Amount :</td>
-         <td>$<?php echo $grand_total; ?></td>
-      </tr>
    </tbody>
    </table>
 
-   <div class="flex">
-      <a href="../index.php?rejected" class="reject-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('Are you sure you want to reject?')">Reject Final Quotation</a>
-      <a href="../index.php?approved" class="approve-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('Are you sure you want to approve?')">Approve of Quotation</a>
-   </div>
-   
 
 </div>
 
 </div>
-<!-- this is for automatic approval for quotations under $5000 -->
-<script type="text/javascript">
-   window.onload = function() {
-      var total = "<?php echo $grand_total; ?>";
-      if (total <= 5000) {
-         var approve = document.getElementsbyName("approve-btn")[0];
-         approve.submit.click();
-      }
-   }
-   
-</script>
 
 </body>
 </html>
