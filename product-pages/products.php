9a62fbc6c28a1d13c3869d1003513d182d9247c2
Karinsarkis98 <91097032+Karinsarkis98@users.noreply.github.com>
2022-12-01 23:22:10 -0500

    .

--- product-pages/products.php
+++ product-pages/products.php
@@ -1,37 +1,54 @@
 <?php
 
 include '../backstore/connection2.php';
-session_start();
-// $user_id = $_SESSION['user_id'];
 
-// if(!isset($user_id)){
-//    header('location:../login-and-signup/login.php');
-// };
+// hardcode username for now
+$user_name = 'karin';
 
-// if(isset($_GET['logout'])){
-//    unset($user_id);
-//    session_destroy();
-//    header('location:../login-and-signup/login.php');
-// };
-//$user_name = $_SESSION['user_name'];
+if(!isset($user_name)){
+   header('location:../login-and-signup/login.php');
+};
+
+$product_name = $_POST['product_name'];
+$product_price = $_POST['product_price'];
+$product_image = $_POST['product_image'];
+$product_quantity = $_POST['product_quantity'];
 
 if(isset($_POST['add_to_cart'])){
 
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-   $product_image = $_POST['product_image'];
-   $product_quantity = $_POST['product_quantity'];
-
-   $select_cart = mysqli_query($conn, "SELECT * FROM `pending` WHERE product_name = '$product_name' AND user_name = 'karin'") or die('query failed');
+   $select_cart = mysqli_query($conn, "SELECT * FROM `pending` WHERE product_name = '$product_name' AND user_name = '$user_name'") or die('query failed');
 
    if(mysqli_num_rows($select_cart) > 0){
       $message[] = 'product already added!';
    }else{
-      mysqli_query($conn, "INSERT INTO `pending`(user_name, product_name, price, image, quantity) VALUES('karin', '$product_name', '$product_price', '$product_image', '$product_quantity')") or die('query failed');
+      mysqli_query($conn, "INSERT INTO `pending`(user_name, product_name, price, image, quantity) VALUES('$user_name', '$product_name', '$product_price', '$product_image', '$product_quantity')") or die('query failed');
       $message[] = 'product added!';
    }
 
 };
+
+if ($conn -> connect_error){
+   die("Connection to the DB failed: ".$conn->connect_error);
+}elseif ($product_quantity*$product_price >= 5000) {
+   $stmt= $conn->prepare("INSERT INTO pending (user_name, product_name, quantity, price, image) VALUES(?,?,?,?,?)");
+   $stmt->bind_param("ssiis",$user_name, $product_name, $product_quantity, $product_price, $product_image);
+   $stmt->execute();
+   echo
+       '<script type="text/javascript">
+   window.onload = function () { alert("RFQ sent for Approval");  location="procurement.php";}
+       </script>';
+}else{
+   $stmt= $conn->prepare("INSERT INTO cart (user_name, product_name, image, quantity, price, status) VALUES(?,?,?,?,?,?)");
+   $stmt->bind_param("sssiis",$user_name, $product_name, $product_quantity, $product_price, $product_image, $status);
+   $result = $stmt->execute();
+
+   if($result){
+       echo "Approved Item Inserted to Cart";
+       header("location: procurement.php");
+   }else{
+       echo "Error moving record to Cart table";
+   }
+}
 
 ?>
 
@@ -62,7 +79,7 @@ require_once ('../header/header.php');
 if(isset($message)){
    foreach($message as $message){
       echo    '<script type="text/javascript">
-      window.click = function () { alert("'.$message.'");}
+      window.onclick = function () { alert("'.$message.'");}
   </script>';
    }
 }
@@ -81,15 +98,14 @@ if(isset($message)){
       if(mysqli_num_rows($select_product) > 0){
          while($fetch_product = mysqli_fetch_assoc($select_product)){
    ?>
-      <form method="post" class="box" action="">
+      <form method="post" class="box" action="../">
          <img src="../images/<?php echo $fetch_product['image']; ?>" alt="">
          <div class="name"><?php echo $fetch_product['product_name']; ?></div>
-         <!-- <div class="price">$<?php echo $fetch_product['price']; ?>/-</div> -->
          <input type="number" min="1" name="product_quantity" value="1">
          <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
          <input type="hidden" name="product_name" value="<?php echo $fetch_product['product_name']; ?>">
          <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+         <input type="submit" value="Generate RFQ" name="add_to_cart" class="btn">
       </form>
    <?php
       };
